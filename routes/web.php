d9c057cd67569c2374b790168a7fe73f5da24b8b
Javier Colmena <1794959@alu.murciaeduca.es>
2023-11-28 09:52:07 +0100

    seguimos con las bases de datos

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\ActividadController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ReconocimientoController;
 use App\Http\Controllers\CurriculoController;
+use App\Models\Estudiante;
 
 /*
 |--------------------------------------------------------------------------
@@ -100,3 +101,54 @@ Route::get('perfil/{id?}', function ($id = null) {
         return "Visualizar el currículo de " . $id;
     }
 })->where('id', '[0-9]+');
+
+
+Route::get('pruebaDB', function () {
+    // aquí irán el código que utiliza el modelo
+    // $estudiante = Estudiante::findOrFail($id);
+
+    // $estudiante = Estudiante::where('votos','>',100)->firstOrFail();
+
+    // Obtener 10 estudiantes con más de 100 votos
+    // $estudiantes = Estudiante::where('votos', '>', 100)->take(10)->get();
+
+    // foreach ($estudiantes as $estudiante) {
+    //     echo $estudiante->nombre . ' ' . $estudiante->votos . '<br />';
+    // }
+
+    // dd($estudiantes); //PARA DEPURAR
+    // echo $estudiante->nombre;
+
+    //INSERTAR
+    // $count = Estudiante::where('votos', '>', 100)->count();
+    // echo 'Antes: ' . $count . '<br />';
+
+    // $estudiante = new Estudiante;
+    // $estudiante->nombre = 'Juan';
+    // $estudiante->apellidos = 'Martínez';
+    // $estudiante->direccion = 'Dirección de Juan';
+    // $estudiante->votos = 130;
+    // $estudiante->confirmado = true;
+    // $estudiante->ciclo = 'DAW';
+    // $estudiante->save();
+
+    // echo 'El estudiante tiene el id: ' . $estudiante->id . '<br/>';
+
+    //ACTUALIZAR
+    // $estudiante = Estudiante::find(42);
+    // $estudiante->votos = 200;
+    // $estudiante->save();
+
+
+    //BORRAR
+    $count = Estudiante::count();
+    echo 'Antes: ' . $count . '<br />';
+    $estudiante = Estudiante::findOrFail(42);
+    $estudiante->delete();
+
+    $count = Estudiante::count();
+    echo 'Después: ' . $count . '<br />';
+
+    // $count = Estudiante::where('votos', '>', 100)->count();
+    // echo 'Después: ' . $count . '<br />';
+});
